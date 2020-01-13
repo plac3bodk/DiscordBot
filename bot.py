cc07d236972b0e68db7b056549d48eba5abda18e
Henrik <vince@doomdigital>
2020-01-13 11:42:37 +0100

    Added connection to client and guild

---
+++ bot.py
@@ -0,0 +1,26 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild members:\n - {members}')
+
+client.run(TOKEN)
