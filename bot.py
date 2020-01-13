ef28c370fcb46126faffe64c3b6be6bc8319bb92
Henrik <vince@doomdigital>
2020-01-13 13:08:49 +0100

    Added private DM for newly joined users

--- bot.py
+++ bot.py
@@ -9,15 +9,16 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} is connected to the following guild:'
-        f'{guild.name}(id: {guild.id})'
-    )
+    print(f'{client.user.name} has connected to Discord!')
 
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild members:\n - {members}')
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to the guild!'
+    )
 
 client.run(TOKEN)
